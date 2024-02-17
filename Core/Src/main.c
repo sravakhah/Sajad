e7821859926d67b04ad600df272b2ddd3368504b
Sajjad Ravakhah <Sravakhah@gmail.com>
2024-02-17 16:26:30 +0330

    Commit 3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -48,6 +48,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define Commit3			3
 #define Commit2			2
 #define MaxLine         3700
 #define MaxMagnet       28
